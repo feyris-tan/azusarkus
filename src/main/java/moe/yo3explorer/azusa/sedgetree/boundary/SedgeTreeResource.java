037b1a6412581c19297c594ad132d236f63ecfe5
Feyris-Tan <ft@feyris-tan.lebtimnetz.de>
2020-05-02 20:27:04 +0200

    Added SedgeTree Version

---
+++ src/main/java/moe/yo3explorer/azusa/sedgetree/boundary/SedgeTreeResource.java
@@ -0,0 +1,33 @@
+package moe.yo3explorer.azusa.sedgetree.boundary;
+
+import io.quarkus.panache.common.Sort;
+import moe.yo3explorer.azusa.sedgetree.entity.VersionEntity;
+import moe.yo3explorer.azusa.web.boundary.RestLicenseService;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.hibernate.Incubating;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/sedgetree")
+public class SedgeTreeResource
+{
+    @Inject
+    RestLicenseService licenseService;
+
+    @Path("/latestVersion")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Tag(name = "sedgetree")
+    public int getLatestVersion(@HeaderParam("Azusa-License") String lic)
+    {
+        licenseService.validateLicenseThrowing(lic);
+
+        VersionEntity ve = VersionEntity.findAll(Sort.descending("id")).firstResult();
+        return ve.id;
+    }
+}
---
+++ src/main/java/moe/yo3explorer/azusa/sedgetree/entity/VersionEntity.java
@@ -0,0 +1,23 @@
+package moe.yo3explorer.azusa.sedgetree.entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import moe.yo3explorer.azusa.azusa.control.ByteArrayBase64Adapter;
+import moe.yo3explorer.azusa.azusa.control.DateUnixtimeAdapter;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+import javax.json.bind.annotation.JsonbTypeAdapter;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.sql.Timestamp;
+
+@Table(name = "versioning", schema = "sedgetree")
+@Entity
+public class VersionEntity extends PanacheEntityBase {
+    @Id
+    public int id;
+    @JsonbTypeAdapter(DateUnixtimeAdapter.class)
+    public Timestamp dateadded;
+    @JsonbTypeAdapter(ByteArrayBase64Adapter.class)
+    public byte[] data;
+}
